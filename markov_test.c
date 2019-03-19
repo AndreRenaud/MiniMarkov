b0693e23b69cff93f4d2fd09ce877606c60cf5aa
Andre Renaud <arenaud@designa-electronics.com>
2019-03-20 09:06:23 +1300

    test: Split text files into words to learn

--- markov_test.c
+++ markov_test.c
@@ -1,37 +1,95 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 #include "markov.h"
+
+static int to_skip(char ch)
+{
+    if (ch == ' ' || ch == '\r' || ch == '\n' || ch == '\"' || ch == '.' || ch == ',')
+        return 1;
+    return 0;
+}
+
+static int next_word(FILE *input, char *buffer, int max_len)
+{
+    int ch;
+    int pos = 0;
+
+    do {
+        ch = fgetc(input);
+        if (ch == EOF)
+            return -1;
+        if (!to_skip(ch))
+            break;
+    } while (1);
+
+    do {
+        if (pos < max_len - 1) {
+            buffer[pos] = ch;
+            pos ++;
+        }
+        ch = fgetc(input);
+        if (ch == EOF)
+            break;
+        if (to_skip(ch))
+            break;
+    } while(1);
+    buffer[pos] = '\0';
+
+
+    if (pos > 1 && buffer[0] == '\'') {
+        memmove(&buffer[1], &buffer[0], pos - 1);
+        pos--;
+    }
+
+    if (pos > 1 && buffer[pos -1] == '\'') {
+        buffer[pos - 1] = '\0';
+        pos--;
+    }
+
+    return pos;
+}
+
+static int learn_file(struct markov_model *model, const char *file)
+{
+    FILE *input = fopen(file, "rb");
+    do {
+        char word[256];
+        if (next_word(input, word, sizeof(word)) < 0)
+            break;
+        //printf("Got word %s\n", word);
+        markov_stream_term(model, word);
+    } while (1);
+    fclose(input);
+
+    markov_flush(model);
+
+    return 0;
+}
 
 int main(int argc, char **argv)
 {
-    struct markov_model *model = markov_generate(NULL, 3);
-    //char *terms[] = {"foo", "blah", "wibble"};
-    //char *terms2[] = {"foo", "blah", "fnord"};
-    char *guess_terms[] = {"foo", "blah"};
-    char *guess_terms2[] = {"foo"};
+    struct markov_model *model = markov_generate(NULL, 4);
 
-    srand(time(NULL));
-    markov_stream_term(model, "foo");
-    markov_stream_term(model, "blah");
-    markov_stream_term(model, "wibble");
-    markov_stream_term(model, "fnord");
-    markov_stream_term(model, "foo");
-    markov_stream_term(model, "blah");
-    markov_stream_term(model, "fnord");
-    markov_stream_term(model, "fnord");
-    markov_stream_term(model, "fnord");
-    markov_stream_term(model, "fnord");
+#if 1 // Sherlock holmes
+    learn_file(model, "texts/a_case_of_identity.txt");
+    learn_file(model, "texts/bosc.txt");
+    learn_file(model, "texts/five.txt");
+    learn_file(model, "texts/blue.txt");
+#endif
+#if 1
+    learn_file(model, "texts/bible.txt");
+#endif
 
-    //markov_add_entry(model, terms);
-    //markov_add_entry(model, terms2);
-    //markov_add_entry(model, terms2);
-    //markov_add_entry(model, terms2);
-    //markov_add_entry(model, terms2);
-
+    printf("DUMP\n");
     markov_dump(model);
 
-    printf("Guess: %s\n", markov_guess_next(model, guess_terms, 2));
-    printf("Guess2: %s\n", markov_guess_next(model, guess_terms2, 1));
+    srand(time(NULL));
+
+    printf("New Story:\n");
+    for (int i = 0; i < 1000; i++)
+        printf("%s ", markov_guess(model));
+    printf("\n");
     return 0;
 }
